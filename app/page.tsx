7f488718e75bf3d558609bf921ec7f45572ccb6a
Alexandre <alexandre01393@gmail.com>
2023-12-31 16:16:39 -0300

    feat: create homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,36 @@
+import { Poppins } from 'next/font/google';
+
+import { LoginButton } from '@/components/auth/login-button';
+import { Button } from '@/components/ui/button';
+
+import { cn } from '@/lib/utils';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['600']
+});
+
 export default function Home() {
-  return <p>Hello Auth</p>;
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <section className="space-y-6 text-center">
+        <h1
+          className={cn(
+            'text-6xl font-semibold text-white drop-shadow-md',
+            poppins.className
+          )}
+        >
+          🔐 Auth
+        </h1>
+        <p className="text-white text-lg">A simple authentication service</p>
+        <article>
+          <LoginButton>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButton>
+        </article>
+      </section>
+    </main>
+  );
 }
